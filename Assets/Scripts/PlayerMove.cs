9d726698d6279ae73a028da1a19de6261b0296b9
Evgeny <evgenykh@me.com>
2023-08-05 19:39:57 +0300

    Squashed commit of the following:
    commit 8ff51c321adcc52f08c398b2f7d10fe3e28ff70c
    Author: Evgeny <evgenykh@me.com>
    Date:   Sat Aug 5 19:13:02 2023 +0300
    
        adapted player's movement to isometric perspective
    
    commit 67710405d4169453762a4b31c1a086aca393c0d9
    Author: Evgeny <evgenykh@me.com>
    Date:   Wed Aug 2 19:56:26 2023 +0300
    
        work in progress
    
    commit 1eeee95517cdf7d35664cfa826390d1abd2998e5
    Author: Evgeny <evgenykh@me.com>
    Date:   Wed Aug 2 19:16:17 2023 +0300
    
        work in progress
    
    commit 9adadd9f5f163ca66bb10f95e7ac5242bd54d010
    Author: KonguKordan <kulintsev.alex@gmail.com>
    Date:   Wed Aug 2 18:57:57 2023 +0300
    
        HOT FIX(changin' crappy tile size)
    
        In this update my fellow gamers we finally fixed all dat shit bout tile size. we r greatful for your patience/ special thanks to our patrons on boosty.com
    
    commit 6b40d5a39165c67c63bb6ca89a5c1b5c7a1c27d8
    Author: Evgeny <evgenykh@me.com>
    Date:   Wed Aug 2 18:40:29 2023 +0300
    
        work in progress
    
    commit a5344c763233d6519adb433f8c883d62d5ba4d8d
    Author: Evgeny <evgenykh@me.com>
    Date:   Fri Jul 28 12:31:08 2023 +0300
    
        work in progress
    
    commit 4675b33b18144e48791261c852559722c5fbe99c
    Author: Evgeny <evgenykh@me.com>
    Date:   Sun Jul 23 12:51:59 2023 +0300
    
        work in progress
    
    commit bc1ef322ec42a04c7201a02a97704c498c76690a
    Author: Evgeny <evgenykh@me.com>
    Date:   Sun Jul 23 12:51:15 2023 +0300
    
        work in progress
    
    commit 96a86873191c2a11d42fea8bfc2fe472cdd7d2cb
    Author: Evgeny <evgenykh@me.com>
    Date:   Sun Jul 9 19:24:22 2023 +0300
    
        pixel-perfect
    
    commit 6473834baffbb40913fe1ae8cde5ec9bcd51c98c
    Author: Evgeny <evgenykh@me.com>
    Date:   Sun Jul 2 21:12:31 2023 +0300
    
        isometric & lightning

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,20 +3,92 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private Tilemap _tilemap;
+    [SerializeField] private Transform _feet;
     private Vector2 _currentMove;
+    private float _jump;
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        _currentMove = context.ReadValue<Vector2>();
+        _currentMove = context.ReadValue<Vector2>().normalized;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        _jump = context.ReadValue<float>();
+    }
+
+    private bool sidebrick;
+
+    void Start()
+    {
+        sidebrick = false;
     }
     
     void Update()
     {
-        transform.position += Time.deltaTime * _moveSpeed * new Vector3(_currentMove.x, _currentMove.y, 0f);
+        Vector3Int cellPosition = _tilemap.WorldToCell(_feet.position);
+        Vector3Int playerMove = new Vector3Int((int)_currentMove.x, (int)_currentMove.y, 0);
+        Vector3Int targetTileCoord = cellPosition + playerMove;
+        TileBase targetTile = _tilemap.GetTile(targetTileCoord);
+
+        if (_tilemap.GetSprite(cellPosition)?.name == "Single_sidebrick_tile" || _tilemap.GetSprite(cellPosition + new Vector3Int(0, 0, 1))?.name == "Single_sidebrick_tile")
+        {
+            sidebrick = true;
+        }
+
+        if (targetTile || sidebrick)
+        {
+            transform.position += Time.deltaTime * _moveSpeed * new Vector3(_currentMove.x, _currentMove.y, 0f);
+        }
+
+        if (sidebrick && (_tilemap.GetSprite(cellPosition)?.name != "Single_sidebrick_tile" || _tilemap.GetSprite(cellPosition + new Vector3Int(0, 0, 1))?.name != "Single_sidebrick_tile"))
+        {
+            sidebrick = false;
+            if (_tilemap.GetTile(_tilemap.WorldToCell(_feet.position + new Vector3Int(0, 0, -1))))
+            {
+                transform.position += new Vector3(0f, 0f, -1f);
+            }
+            else if (_tilemap.GetTile(_tilemap.WorldToCell(cellPosition)))
+            {
+                goto SKIP;
+            }
+            else if (_tilemap.GetTile(_tilemap.WorldToCell(_feet.position + new Vector3Int(0, 0, 1))))
+            {
+                transform.position += new Vector3(0f, 0f, 1f);
+            }
+        }
+
+        SKIP:   ;
+
+        /*
+        Vector3Int cellPosition = _tilemap.WorldToCell(_feet.position);
+        Vector3Int playerMove = new Vector3Int((int)_currentMove.x, (int)_currentMove.y, 0);
+        Vector3Int targetTileCoord = cellPosition + playerMove;
+        TileBase targetTile = _tilemap.GetTile(targetTileCoord);
+        if (targetTile)
+        {
+            transform.position += Time.deltaTime * _moveSpeed * new Vector3(_currentMove.x, _currentMove.y, 0f);
+        }
+
+        Vector3Int targetTileBelowCoord = new Vector3Int(targetTileCoord.x, targetTileCoord.y, targetTileCoord.z - 1);
+        TileBase targetTileBelow = _tilemap.GetTile(targetTileBelowCoord);
+        if (_tilemap.GetSprite(cellPosition)?.name == "Single_sidebrick_tile" && targetTileBelow)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, targetTileBelowCoord.z);
+        }
+           
+        Vector3Int targetTileAboveCoord = new Vector3Int(targetTileCoord.x, targetTileCoord.y, targetTileCoord.z + 1);
+        TileBase targetTileAbove = _tilemap.GetTile(targetTileAboveCoord);
+        if (_tilemap.GetSprite(cellPosition + new Vector3Int(0, 0, 1))?.name == "Single_sidebrick_tile" && targetTileAbove)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, targetTileAboveCoord.z);
+        }*/
     }
 
     public void SavePlayerPosition()
